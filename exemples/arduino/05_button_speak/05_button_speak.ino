e631091bbf1a23f041da68481675ab668b80ff90
Paul Lëon <hello@paulllleon.ch>
2021-03-22 15:38:57 +0100

    update

--- exemples/arduino/05_button_speak/05_button_speak.ino
+++ exemples/arduino/05_button_speak/05_button_speak.ino
@@ -1,6 +1,4 @@
-String cmd = "";
 int button_pin = 5;
-
 #define msg "b1"
 
 boolean readyToPush = true;
@@ -9,28 +7,19 @@ void setup()
 {
   Serial.begin(9600);
   delay(1000);
-  Serial.setTimeout(500); // more timeout if long messages
+  Serial.setTimeout(10); // more timeout if long messages
   Serial.print("Board available");
-  delay(500);
   Serial.print("press the button to test");
 
   pinMode(button_pin, INPUT_PULLUP);
 }
 void loop()
 {
-
-  //  cmd = Serial.readString();
-  //  if (cmd != "") {
-  //    Serial.print(cmd);
-  //  }
-
-  // button
   int buttonState = digitalRead(button_pin);
   if (buttonState == LOW && readyToPush)
   {
     Serial.print(msg);
     readyToPush = false;
-    //delay(2000); // wait for 2 sec
   }
   else if (buttonState == HIGH)
   {
