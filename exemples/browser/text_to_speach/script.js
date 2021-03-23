f1bb1a2f8241ef6ba25a48a3dfc2b408dae17760
Paul Lëon <hello@paulllleon.ch>
2021-03-23 09:10:29 +0100

    update

--- exemples/browser/text_to_speach/script.js
+++ exemples/browser/text_to_speach/script.js
@@ -1,13 +1,17 @@
 var press_counter = 1;
 
+var synth = window.speechSynthesis
+var voices;
+
 function processCommand(serial_cmd) {
     var text_msg = "";
     if (serial_cmd =="1"){
-        console.log("counter = "+ press_counter);
+        //console.log("counter = "+ press_counter);
         if (press_counter == 1){
             text_msg ="you just pressed me";
         }else if (press_counter == 2){
             text_msg ="you pressed me again!";
+            //text_msg ="Je parle français";
         }else if (press_counter == 5){
             text_msg ="you pressed me again, i feel very pressed!";
         }else if (press_counter == 10){
@@ -24,20 +28,43 @@ function processCommand(serial_cmd) {
 }
 
 function speakMessage(msg_to_speak) {
-    var synth = window.speechSynthesis
+    
     var msg = new SpeechSynthesisUtterance();
-    //var voices = synth.getVoices();
 
-    msg.rate = 0.6; // From 0.1 to 10
+    // if(voices != 0){
+    //     msg.voice = voices[13];
+    //     msg.rate = 1;
+    // }
+
+   // msg.rate = 0.6; // From 0.1 to 10
     msg.pitch = 1; // From 0 to 2
     msg.lang = 'en-US';
     //msg.voice = voices[12];
     msg.text = msg_to_speak;
+    //
+//console.log(voices);
     synth.speak(msg);
+    
 
     msg.onend = function(event) {
         //console.log('Utterance has finished being spoken after ' + event.elapsedTime + ' milliseconds.');
         sendToArduino("10"); // send code 10: voice finished to speak
+        console.log("sended 10")
     } 
-    //console.log(voices);
+    
 }
+
+window.onload = ()=>{
+    
+    synth.onvoiceschanged = ()=>{
+        voices = synth.getVoices();
+        console.log("ready");
+        console.log(voices)
+    }
+   // voices = synth.getVoices();
+   
+}
+
+
+
+
